ecc818bb22fd1dac7221a391d3c38ab9c38a95cc
John Rey Tepacia <johnreytepacia.utech@gmail.com>
2025-01-03 16:15:52 +0800

    bulding tour overview

--- controllers/viewsController.js
+++ controllers/viewsController.js
@@ -1,11 +1,19 @@
-exports.getOverview = (req, res) => {
-    res.status(200).render('overview', {
-        title: 'All Tours'
-    });
-}
+const Tour = require('./../models/tourModel');
+const catchAsync = require('./../utils/catchAsync');
 
-exports.getTour = (req, res) => {
+exports.getOverview = catchAsync(async (req, res, next) => {
+
+    const tours = await Tour.find();
+    
+    res.status(200).render('overview', {
+        title: 'All Tours',
+        tours
+    });
+
+});
+
+exports.getTour = catchAsync( async (req, res, next) => {
     res.status(200).render('tour', {
         title: 'The Forest Hiker'
     });
-}
+});
