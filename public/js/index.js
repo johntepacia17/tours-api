9a22ea94377ef17145513a7a1bb7ceaba89c68ab
John Rey Tepacia <johnreytepacia.utech@gmail.com>
2025-01-07 23:44:50 +0800

    create functionalities for updating user with api

--- public/js/index.js
+++ public/js/index.js
@@ -1,12 +1,13 @@
 import '@babel/polyfill';
 import {login, logout} from './login';
 import {displayMap} from './mapbox';
+import {updateData} from './updateSettings';
 
 // DOM elements
 const mapBox = JSON.parse(document.getElementById('map'));
 const loginForm = document.querySelector('.form--login');
 const logOutBtn = document.querySelector('.nav__el--logout');
-
+const userDataForm = document.querySelector('.form-user-data');
 
 
 // delegations
@@ -26,3 +27,12 @@ if(loginForm) {
 }
 
 if(logOutBtn) logOutBtn.addEventListener('click', logout);
+
+if(userDataForm) {
+    userDataForm.addEventListener('submit', e => {
+        e.preventDefault();
+        const name = document.getElementById('name').value;
+        const email = document.getElementById('email').value;
+        updateData(name, email);
+    });
+}
---
+++ public/js/updateSettings.js
@@ -0,0 +1,25 @@
+import axios from 'axios';
+import { showAlert } from './alert';
+
+export const updateData = async (name, email) => {
+
+    try {
+        
+        const res = await axios({
+            method: 'PATCH',
+            url: 'http://127.0.0.1:3000/api/v1/users/update-me',
+            data: {
+                name,
+                email
+            }
+        });
+
+        if(res.data.status === 'Success') {
+            showAlert('success', 'Successfully updated!');
+        }
+
+    } catch(err) {
+        showAlert('error', err.response.data.message);
+    }
+
+}
