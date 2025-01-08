71cf29a39793722aa0ffdd57b8fb30fdede06434
John Rey Tepacia <johnreytepacia.utech@gmail.com>
2025-01-08 22:07:15 +0800

    install sharp and create photo resizer middleware

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -19,7 +19,12 @@ router.use(authController.protect);
 router.get('/me', userController.getMe, userController.getUser);
 router.patch('/update-password', authController.updatePassword);
 
-router.patch('/update-me', userController.uploadUserPhoto, userController.updateMe);
+router.patch(
+    '/update-me', 
+    userController.uploadUserPhoto, 
+    userController.resizeUserPhoto, 
+    userController.updateMe
+);
 
 router.delete('/delete-me', userController.deleteMe);
 
