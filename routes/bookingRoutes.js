8e813f8a29acf982fcfb29d3312c1da1ca5805d7
John Rey Tepacia <johnreytepacia.utech@gmail.com>
2025-01-16 01:45:36 +0800

    creating endpoints for bookings

--- routes/bookingRoutes.js
+++ routes/bookingRoutes.js
@@ -5,8 +5,26 @@ const authController = require('./../controllers/authController');
 // allow router to merge from tourRoutes
 const router = express.Router();
 
+// only accessible to login users
+router.use(authController.protect);
+
 // this route is only for the client just to get the checkout sessions
 router.get('/checkout-session/:tourId', authController.protect, bookingController.getCheckoutSession);
+
+// only accessible to admin and lead guides
+router.use(authController.restrictTo('admin', 'lead-guide'))
+
+router
+    .route('/')
+    .get(bookingController.getAllBookings)
+    .post(bookingController.createBooking);
+
+router
+    .route('/:id')
+    .get(bookingController.getBooking)
+    .patch(bookingController.updateBooking)
+    .delete(bookingController.deleteBooking);
+
 
 
 module.exports = router;
